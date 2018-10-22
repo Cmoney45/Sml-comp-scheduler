38b760aa24265fd9a7da015dee35ad3319d54e5a
Cmoney <cascherer42@gmail.com>
2018-10-21 19:09:40 -0500

    Add a few routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,4 @@
-const db = require("../models");
-
 module.exports = app => {
-  // Get all examples
-  app.get("/api/examples", (req, res) => {
-    db.Example.findAll({}).then(dbExamples => {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", (req, res) => {
-    db.Example.create(req.body).then(dbExample => {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", (req, res) => {
-    db.Example.destroy({ where: { id: req.params.id } }).then(dbExample => {
-      res.json(dbExample);
-    });
-  });
+  require("./apiRoutes/companyApiRoutes")(app);
+  require("./apiRoutes/userApiRoutes")(app);
 };
