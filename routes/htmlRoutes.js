bb4885b13a8262818c11b468949984fe1b81403f
Cmoney <cascherer42@gmail.com>
2018-10-26 17:51:59 -0500

    Create demo routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,30 @@ const db = require("../models");
 
 module.exports = app => {
   // Load index page
-  app.get("/", (req, res) => {
-    db.User.findAll({}).then(User => {
-      res.render("index", {
-        msg: "Welcome!",
-        users: User
+  app.get("/company/:CompanyId/:userId/:isManager", (req, res) => {
+    const { CompanyId } = req.params;
+    const { isManager } = req.params;
+    const { userId } = req.params;
+
+    if (isManager) {
+      db.User.findAll({
+        where: { CompanyId }
+      }).then(User => {
+        res.render("index", {
+          msg: "Welcome!",
+          users: User
+        });
       });
-    });
+    } else {
+      db.User.findOne({
+        where: { CompanyId, userId }
+      }).then(User => {
+        res.render("index", {
+          msg: "Welcome!",
+          users: User
+        });
+      });
+    }
   });
 
   app.get("/adduser", (req, res) => {
@@ -32,7 +49,7 @@ module.exports = app => {
   // Render the index page for any unmatched routes
   app.get("*", (req, res) => {
     db.User.findAll({}).then(User => {
-      res.render("index", {
+      res.render("login", {
         msg: "Welcome!",
         users: User
       });
