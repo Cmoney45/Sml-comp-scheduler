c55390a1f42ee66a72ba081d11be2dcd3acc297d
Casttle <cberry1222@gmail.com>
2018-10-26 20:27:45 -0500

    changes to the table functions and styles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -24,8 +24,10 @@ module.exports = app => {
   });
 
   app.get("/schedule", (req, res) => {
-    res.render("schedule", {
-      schedule: Schedule
+    db.Scheduler.findAll({}).then(Scheduler => {
+      res.render("schedule", {
+        schedule: Scheduler
+      });
     });
   });
 
